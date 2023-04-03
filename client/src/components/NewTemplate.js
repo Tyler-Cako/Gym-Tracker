eb54e86a5ffe2b4869580eb423604490cf7bd14a
Tyler <tylercakotc@gmail.com>
2023-04-03 02:54:54 -0600

    Dynamic form not yet working

--- client/src/components/NewTemplate.js
+++ client/src/components/NewTemplate.js
@@ -1,16 +1,34 @@
+import { useState } from 'react'
 import TemplateExercise from './TemplateExercise'
 
 function NewTemplate() {
+    const [ exercises, setExercises ] = useState([{name: "", reps: 0, sets: 0}])
+
     const handleSubmit = (e) => {
         e.preventDefault()
+    }
+
+    const addExercise = (e) => {
+        e.preventDefault()
+
+        let state = exercises
+        state.push({name: "", reps: 0, sets: 0})
+        setExercises(state)
+        console.log(exercises)
     }
 
     return(
         <>
             <div className="u-cf"></div>
             <form className="workout-form">   
-                <TemplateExercise />
-                <button>Add Exercise</button>
+                <tbody>
+                    {exercises.map(({name, reps, sets}) => {
+                        return (
+                            <TemplateExercise name={name} reps={reps} sets={sets}/>
+                        )
+                    })}
+                    <button onClick={addExercise}>Add Exercise</button>
+                </tbody>
             </form>
         </>
     )
--- client/src/components/TemplateExercise.js
+++ client/src/components/TemplateExercise.js
@@ -1,17 +1,23 @@
-const TemplateExercise = ({ name, reps, sets }) => {
+import { useState } from 'react'
+
+const TemplateExercise = (props) => {
+    const [ name, setName ] = useState(props.name)
+    const [ reps, setReps ] = useState(props.reps)
+    const [ sets, setSets ] = useState(props.sets)
+
     return (
         <div className="row u-full-width">
             <div className="eight columns">
                 <label for="name">Exercise name</label>
-                <input type="text" name="name" className="u-full-width" value={name}/>
+                <input type="text" name="name" className="u-full-width" value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className="two columns">
                 <label for="reps">Reps</label>
-                <input type="number" name="reps" className="u-full-width" value={name}/>
+                <input type="number" name="reps" className="u-full-width" value={reps} onChange={(e) => setReps(e.target.value)}/>
             </div>
             <div className="two columns">
                 <label for="sets">Sets</label>
-                <input type="number" name="sets" className="u-full-width" value={name}/>
+                <input type="number" name="sets" className="u-full-width" value={sets} onChange={(e) => setSets(e.target.value)}/>
             </div>
         </div>
     )
